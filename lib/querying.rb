778648f0c6731255e59b8e7ae7100a5709c9358f
Jul Jen <juliajen.prog@gmail.com>
2018-03-20 17:41:18 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -37,6 +37,5 @@ def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name as name, COUNT(books.series_id)
   FROM characters
   INNER JOIN books
-  ON characters.series_id = books.series_id
-  GROUP BY books.series_id";
+  ON characters.series_id = books.series_id";
 end
