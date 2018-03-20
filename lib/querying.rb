db45d2d04079391f7259c4bb1a487f3313f81f7c
Jul Jen <juliajen.prog@gmail.com>
2018-03-20 17:04:01 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,7 +12,7 @@ def select_name_and_motto_of_char_with_longest_motto
 end
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT characters.species as species
+  "SELECT characters.species as species, COUNT(species) as count
   FROM characters
   ORDER BY(count) DESC LIMIT 1";
 end
