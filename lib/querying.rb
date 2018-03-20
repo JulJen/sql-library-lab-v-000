90120a145f1ff92e7ef40919fa6277d4e662c41e
Jul Jen <juliajen.prog@gmail.com>
2018-03-20 17:20:29 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -34,9 +34,9 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name as name, author
+  "SELECT characters.name as name, COUNT(name)
   FROM characters
   INNER JOIN authors
   ON characters.author_id = authors.id
-  ORDER BY characters.species DESC LIMIT 1";
+  ORDER BY name DESC";
 end
