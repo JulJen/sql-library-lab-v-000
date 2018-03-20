fd20683a00c4c3e2a9422e9e10b3841f1c926324
Jul Jen <juliajen.prog@gmail.com>
2018-03-20 17:26:42 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -38,5 +38,5 @@ def select_character_names_and_number_of_books_they_are_in
   FROM characters
   INNER JOIN books
   ON characters.series_id = books.series_id
-  ORDER BY number DESC";
+  ORDER BY name DESC";
 end
