3eca4a0568282407b099def0f874a7c0f63d482f
Jul Jen <juliajen.prog@gmail.com>
2018-03-20 17:10:32 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -8,13 +8,13 @@ end
 def select_name_and_motto_of_char_with_longest_motto
   "SELECT characters.name as name, characters.motto as motto
   FROM characters
-  ORDER BY(name) DESC LIMIT 1";
+  ORDER BY name DESC LIMIT 1";
 end
 
 def select_value_and_count_of_most_prolific_species
   "SELECT characters.species as value, COUNT(characters.species) as count
   FROM characters
-  ORDER BY(value)";
+  GROUP BY value ";
 end
 
 def select_name_and_series_subgenres_of_authors
