e7619119b0f0aa7d3b9898340267b5f3754858d5
Jul Jen <juliajen.prog@gmail.com>
2018-03-20 17:11:19 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -15,7 +15,7 @@ def select_value_and_count_of_most_prolific_species
   "SELECT characters.species as value, COUNT(characters.species) as count
   FROM characters
   GROUP BY value
-  ORDER BY count DESC";
+  ORDER BY count DESC LIMIT 1";
 end
 
 def select_name_and_series_subgenres_of_authors
