7a77caaa28bae2b1a7992c418dab9273f02545f2
Jul Jen <juliajen.prog@gmail.com>
2018-03-20 17:15:13 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -26,7 +26,11 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title"
+  "SELECT series.title
+  FROM series
+  INNER JOIN characters
+  ON series.id = characters.series_id
+  HAVING select_value_and_count_of_most_prolific_species";
 end
 
 def select_character_names_and_number_of_books_they_are_in
